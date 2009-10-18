e6bce220c0c32d7eb7b5edb1ad7de0ba09f1547c
mauro <mauro@7b85a724-8460-4c5b-b9d8-b8606838f8a8>
2009-10-18 17:30:01 +0000

    pre tag 1.0.0

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ version = '1.0.0'
 setup(name='plone4bio.pscoils',
       version=version,
       description="plone4bio: pscoils predictor",
-      long_description=open(os.path.join("plone4bio", "pscoils", "README.txt")).read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open(os.path.join("plone4bio", "pscoils", "README.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
